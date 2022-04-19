3a33a846826d67430c82ad7a7db4306103e7a768
ajmalappas <ajmal.appas@anywher.co>
2022-04-19 11:49:43 +0530

    webapp-codes

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import '../scss/main.scss';
+// import './icons/arrow-icon.svg';
+// import './icons/arrow-right.svg';
+// import './icons/bed-icon.svg';
+// import './icons/chevron-icon.svg';
+// import './icons/search-icon.svg';
+// import './icons/shower-icon.svg';
+
+const ajax = new XMLHttpRequest();
+ajax.open("GET", "./spritemap.svg", true);
+ajax.send();
+ajax.onload = function(e) {
+  const div = document.createElement("div");
+  div.innerHTML = ajax.responseText;
+  document.body.insertBefore(div, document.body.childNodes[0]);
+  div.classList.add('hide-svg');
+}
---
+++ webpack.config.js
@@ -0,0 +1,151 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SvgSpritemap = require('svg-spritemap-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const ImageminPlugin = require("imagemin-webpack");
+
+
+
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+// const { extendDefaultPlugins } = require("svgo");
+
+module.exports = {
+  mode:'development',
+    entry:{
+      main:path.resolve(__dirname,'src/js/index.js')
+    },
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    publicPath: '/dist',
+    filename:'[name].js',
+  },
+
+  module:{
+    rules:[
+      {
+        test:/\.scss$/,
+        use:[MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']          
+      },
+
+
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options:{
+      //         outputpath: 'images',
+      //         publicPath: 'images',
+      //         emitfile: true,
+
+      //       }
+      //     },
+      //     {
+      //       loader: ImageminPlugin.loader,
+      //       options: {
+      //         bail: true,
+      //         imageminOptions: {
+      //           plugins: [
+      //             ["pngquant",{quality : [0.5, 0.5]}],
+      //             ["mozjpeg",{quality : 50, progressive:true }],
+      //             ["gifsicle",{interlaced:true, optimizationlevel:3 }],
+      //             })
+      //           ]
+      //         }
+      //       }
+      //     }
+      //   ]
+      // }
+
+    ]
+  },
+  
+  plugins:[
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+
+    new HtmlWebpackPlugin({  
+      filename: "index.html",
+      template: './src/index.html'
+    }),
+
+    new CleanWebpackPlugin(),
+
+    new SvgSpritemap('./src/icons/*.svg', {
+      output: {
+        filename: 'spritemap.svg',
+        svg: {sizes: false},
+        svg4everybody: true,
+        svgo: true,
+      },
+      sprite: {
+        prefix: false,
+        gutter: 0,
+        generate: {
+          title: false,
+          symbol: true
+        },
+      },
+    }),
+  ],
+
+  
+
+  optimization: {
+    minimizer: [
+
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              // [
+              //   "svgo",
+              //   {
+              //     plugins: extendDefaultPlugins([
+              //       {
+              //         name: "removeViewBox",
+              //         active: false,
+              //       },
+              //       {
+              //         name: "addAttributesToSVGElement",
+              //         params: {
+              //           attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+              //         },
+              //       },
+              //     ]),
+              //   },
+              // ],
+            ],
+          },
+        },
+      }),
+    ]
+  },
+
+
+  devServer: {
+    static: {
+      publicPath: '',
+    },
+    compress: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    port: 2002,
+    watchFiles: {
+      paths: ['./src/*.html', 'dist/**/*'],
+      options: {
+        usePolling: false,
+      },
+    },
+  },
+
+ 
+};
